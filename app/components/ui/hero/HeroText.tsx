9749e53072f5c42becf12006500679df140e90a1
danieleardizzone <daniele.ardi02@gmail.com>
2025-03-10 23:50:50 +0100

    EDIT - hero style

--- app/components/ui/hero/HeroText.tsx
+++ app/components/ui/hero/HeroText.tsx
@@ -7,10 +7,10 @@ export default function HeroText() {
         <div className="flex flex-col gap-2 sm:gap-5 lg:gap-12
                         px-4 sm:px-0
                         text-secondary-content">
-            <h1 className="text-center text-2xl md:text-4xl lg:text-5xl font-bold">
+            <h1 className="text-center text-nowrap text-2xl md:text-4xl lg:text-5xl font-bold">
                 Flowers Apartments
             </h1>
-            <p className="text-xs md:text-sm lg:text-lg font-bold">
+            <p className="text-center text-xs md:text-sm lg:text-lg font-bold">
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident praesentium ut laudantium nisi iste nemo maxime repellat aliquid, nam quidem.
             </p>
             <div className="mx-auto">
