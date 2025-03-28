dc4335b329156dac76f8efcf670c25de865f07d0
danieleardizzone <daniele.ardi02@gmail.com>
2025-03-28 19:35:00 +0100

    EDIT - sectionTitle size & container style

--- app/components/ui/Container.tsx
+++ app/components/ui/Container.tsx
@@ -1,7 +1,7 @@
 
 export default function Container({ children }: { children: React.ReactNode }) {
     return (
-        <div className="container mx-auto mb-4 px-1 md:px-0">
+        <div className="container mx-auto pb-4 px-1 md:px-0">
             {children}
         </div>
     )
--- app/components/ui/SectionTitle.tsx
+++ app/components/ui/SectionTitle.tsx
@@ -1,6 +1,6 @@
 export default function SectionTitle({ children }: { children: React.ReactNode }) {
     return (
-        <div className="text-xl sm:text-2xl md:text-4xl lg:text-5xl text-accent font-extrabold">
+        <div className="text-xl sm:text-2xl md:text-4xl lg:text-5xl text-center text-accent font-extrabold pb-2">
             {children}
         </div>
     )
