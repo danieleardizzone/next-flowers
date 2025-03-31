cb391a6b75920afaef828c5a4e0fdb2cd8838788
danieleardizzone <daniele.ardi02@gmail.com>
2025-03-31 21:38:50 +0200

    EDIT - section title text-size

--- app/components/ui/SectionTitle.tsx
+++ app/components/ui/SectionTitle.tsx
@@ -1,6 +1,6 @@
 export default function SectionTitle({ children }: { children: React.ReactNode }) {
     return (
-        <div className="text-xl sm:text-2xl md:text-4xl lg:text-5xl text-center text-accent font-extrabold pb-2">
+        <div className="text-2xl sm:text-3xl md:text-4xl lg:text-5xl text-center text-accent font-extrabold pb-2">
             {children}
         </div>
     )
