f011a605a479d292a470af9aa6b0c05a80618d3c
danieleardizzone <daniele.ardi02@gmail.com>
2025-03-31 21:53:55 +0200

    ADD - Activities section

---
+++ app/components/ui/activities/Activities.tsx
@@ -0,0 +1,20 @@
+import Container from "../LgContainer";
+import SectionTitle from "../SectionTitle";
+
+export default function Activities() {
+    return (
+        <div className="bg-primary text-primary-content p-2">
+            <Container>
+                <SectionTitle>
+                    <h2>
+                        Cosa fare a Marineo
+                    </h2>
+                </SectionTitle>
+
+                <div className="p-1 md:p-2">
+                    Scopri la bellezza e la tranquillità di Marineo, un incantevole borgo siciliano situato nel cuore della provincia di Palermo. La nostra casa vacanze è il luogo ideale per una fuga rilassante immersa nella natura e nella cultura locale. Nelle vicinanze di Marineo è possibile scoprire posti meravigliosi e prendere parte ad attività senza eguali.
+                </div>
+            </Container>
+        </div>
+    )
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import HeroSection from "@/app/components/ui/hero/HeroSection";
 import About from "@/app/components/ui/about/About";
+import Activities from "@/app/components/ui/activities/Activities";
 
 export default function Home() {
   return (
     <div>
       <HeroSection />
       <About />
+      <Activities />
     </div>
   );
 }
