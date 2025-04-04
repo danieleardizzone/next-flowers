e8bca6ed845543582b85e1dc9a937e893fbbe6e0
danieleardizzone <daniele.ardi02@gmail.com>
2025-04-04 20:19:18 +0200

    EDIT - for merge errors

--- app/booking/page.tsx
+++ app/booking/page.tsx
@@ -13,11 +13,8 @@ export interface EventData {
     ortensia: Event[],
 }
 
-<<<<<<< HEAD
 export const dynamic = 'force-dynamic';
 
-=======
->>>>>>> e94fe4e02e7d8e7a81075bfc013ceed726b28a2c
 export default async function Page() {
 
     const res = await fetch('http://localhost:3000/api/getCalendars', { cache: 'no-store' });
