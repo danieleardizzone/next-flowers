0ab03d61cbe1da4463b99d6a2fd3d22731243cc9
danieleardizzone <daniele.ardi02@gmail.com>
2025-04-10 15:34:20 +0200

    REMOVE - LgContainer - useless

--- app/components/ui/LgContainer.tsx
+++
@@ -1,7 +0,0 @@
-export default function Container({ children }: { children: React.ReactNode }) {
-    return (
-        <div className="lg:container lg:mx-auto mb-4 px-1 md:px-0">
-            {children}
-        </div>
-    )
-}
